bd1d1f9520b82a7b3890cb693c3b5a6121c8c01e
bjaehyun <bjaehyun@amazon.com>
2020-03-21 23:57:21 +0900

    npm run amplify-modelgen

---
+++ src/models/schema.js
@@ -0,0 +1,40 @@
+export const schema = {
+    "models": {
+        "Message": {
+            "syncable": true,
+            "name": "Message",
+            "pluralName": "Messages",
+            "attributes": [
+                {
+                    "type": "model",
+                    "properties": {}
+                }
+            ],
+            "fields": {
+                "id": {
+                    "name": "id",
+                    "isArray": false,
+                    "type": "ID",
+                    "isRequired": true,
+                    "attributes": []
+                },
+                "title": {
+                    "name": "title",
+                    "isArray": false,
+                    "type": "String",
+                    "isRequired": true,
+                    "attributes": []
+                },
+                "color": {
+                    "name": "color",
+                    "isArray": false,
+                    "type": "String",
+                    "isRequired": false,
+                    "attributes": []
+                }
+            }
+        }
+    },
+    "enums": {},
+    "version": "6be53ee48c0f3c079b9ef792c84a7d19"
+};
